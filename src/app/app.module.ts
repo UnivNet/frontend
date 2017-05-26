4211ca24183357cbfc6a7cef76462711af6eb05a
Vivek Roy <vivekroyandroid@gmail.com>
2017-05-26 16:29:56 +0530

    Add components: student-profile, admin-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
+import { AdminProfileComponent } from './admin-profile/admin-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentProfileComponent,
+    AdminProfileComponent
   ],
   imports: [
     BrowserModule,
